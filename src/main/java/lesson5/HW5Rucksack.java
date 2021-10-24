b99095902fe5dfac44c149f365a6f326742a89dc
upagka <upagka@yandex.com>
2021-10-24 11:39:54 +0600

    Complete 5 homework.

--- src/main/java/lesson5/HW5Rucksack.java
+++ src/main/java/lesson5/HW5Rucksack.java
@@ -1,57 +1,63 @@
 package lesson5;
 
-import java.sql.SQLOutput;
-import java.util.HashMap;
+
 import java.util.HashSet;
-import java.util.Set;
 
 public class HW5Rucksack {
 
 
     public static void main(String[] args) {
-        Set<Thing> things = new HashSet<>();
+        HashSet<Thing> things = new HashSet<>();
         things.add(new Thing("Колбаса", 4,1000));
         things.add(new Thing("Гречка", 1,8000));
         things.add(new Thing("Ноутбук", 3,7000));
         things.add(new Thing("Тостер", 5,1200));
-        things.add(new Thing("Гантеля", 10,500));
+        things.add(new Thing("Гантеля", 10,1500));
         things.add(new Thing("Телевизор", 8,5000));
-        int maxWeight = 30;
+        int maxWeight = 4;
         System.out.println(bestThings(things, maxWeight));
     }
 
-    private static HashSet bestThings(Set<Thing> things, int maxWeight) {
-        if (things == null){
-            return null;
+    private static HashSet bestThings(HashSet<Thing> things, int maxWeight) {
+
+        //Если набор пустой
+        if (things.isEmpty()){
+            return things;
         }
-        int sum = 0;
-        for (Thing thing : things) {
-            sum = sum + thing.getWeight();
+
+        //Если набор входит в максимальный вес
+        if (countWeight(things) <= maxWeight)
+            return things;
+
+        //Если набор из 1 вещи (но она тяжелее максимального веса)
+        if (things.size() == 1){
+            return new HashSet();
         }
-        if (sum <= maxWeight){
-            return (HashSet) things;
-        }
-        Thing pantry = null;
-        Thing without = null;
-        int max = 0;
+
+        // Случай, когда набор состоит из 2+ вещей и тяжелее максимального веса.
         int maxPrice = 0;
-        Set<Thing> copy = new HashSet<>(things);
+        HashSet<Thing> copy = new HashSet<>(things); //Копия, чтобы не изменять things в итераторе.
+        HashSet<Thing> theBest = new HashSet<>(); //Сохраняем сюда лучший из результатов
+        HashSet<Thing> temp = new HashSet<>(); //Результат вызова рекурсии
         for (Thing thing : things) {
             copy.remove(thing);
-            if (countWeight((HashSet<Thing>) copy) <= maxWeight && countPrice((HashSet<Thing>) copy) > maxPrice){
-                without = new Thing(thing.getName(),thing.getWeight(),thing.getPrice());
-                max = countWeight((HashSet<Thing>) copy);
-                maxPrice = countPrice((HashSet<Thing>) copy);
-
+            temp.clear();
+            temp.addAll(bestThings(copy, maxWeight));
+            if (!temp.isEmpty()) {
+                if (countPrice(temp) > maxPrice){
+                    theBest.clear();
+                    theBest.addAll(temp);
+                    maxPrice = countPrice(theBest);
+                }
             }
             copy.add(thing);
         }
-        if(max != 0){
-            System.out.println(without);
-            things.remove(without);
-            return (HashSet) things;
+        if (maxPrice > 0)
+        {
+            System.out.println(maxPrice);
+            return theBest;
         }
-        return  null;
+        return new HashSet();
     }
 
     private static int countPrice(HashSet<Thing> set) {
