01002a472f88a721f93c6849d3c61d7381b78502
upagka <upagka@yandex.com>
2021-10-31 18:26:36 +0600

    Homework 6 Start

---
+++ src/main/java/Lesson7/Main.java
@@ -0,0 +1,63 @@
+package Lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+//        testGraph();
+        testDfs();
+        testBfs();
+    }
+
+    private static void testGraph() {
+        Graph graph = new GraphImpl(4);
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+
+        graph.addEdge("A", "B", "C");
+        graph.addEdge("B", "C", "D");
+        graph.addEdge("C", "A", "B", "D");
+        graph.addEdge("D", "B", "C");
+
+        System.out.println("Size of graph is " + graph.getSize());
+        graph.display();
+    }
+
+    private static void testDfs() {
+        Graph graph = new GraphImpl(7);
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+
+        graph.addEdge("A", "B", "C", "D");
+        graph.addEdge("B", "E");
+        graph.addEdge("D", "F");
+        graph.addEdge("F", "G");
+
+        graph.dfs("A");
+    }
+
+    private static void testBfs() {
+        Graph graph = new GraphImpl(8);
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+        graph.addVertex("H");
+
+        graph.addEdge("A", "B", "C", "D");
+        graph.addEdge("B", "E");
+        graph.addEdge("E", "H");
+        graph.addEdge("C", "F");
+        graph.addEdge("D", "G");
+
+        graph.bfs("A");
+    }
+}
